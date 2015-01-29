ff9aa086205b87daed38bac91bd8240f8db3f27d
Pooyan Khosravi <pekhee@gmail.com>
2015-01-29 17:17:30 +0800

    bump cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "amir@evolvingweb.ca"
 license          "Apache 2.0"
 description      "Installs/Configures/Bootsraps Drupal"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 
 depends "apt"
 depends "build-essential"
